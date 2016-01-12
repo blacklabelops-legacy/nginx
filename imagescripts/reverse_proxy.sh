444646ad61a6fef6f38ef8cc4666658bffcd47dd
Steffen Bleul <blacklabelops@itbleul.de>
2016-01-12 10:58:50 +0100

    Disabled redirection.

--- imagescripts/reverse_proxy.sh
+++ imagescripts/reverse_proxy.sh
@@ -17,6 +17,7 @@ _EOF_
   if [ -n "${NGINX_PROXY_PASS}" ]; then
     cat >> ${NGINX_DIRECTORY}/nginx.conf <<_EOF_
           proxy_pass ${NGINX_PROXY_PASS};
+          proxy_redirect off;
 _EOF_
   fi
 
