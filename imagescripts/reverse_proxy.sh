18ca5a730a67dcdda6f750dcb1271155c09a07f9
Steffen Bleul <blacklabelops@itbleul.de>
2016-01-12 11:20:20 +0100

    Trying different setting to turn off local ports in redirects.

--- imagescripts/reverse_proxy.sh
+++ imagescripts/reverse_proxy.sh
@@ -17,7 +17,7 @@ _EOF_
   if [ -n "${NGINX_PROXY_PASS}" ]; then
     cat >> ${NGINX_DIRECTORY}/nginx.conf <<_EOF_
           proxy_pass ${NGINX_PROXY_PASS};
-          proxy_redirect off;
+          proxy_redirect / /;
 _EOF_
   fi
 
