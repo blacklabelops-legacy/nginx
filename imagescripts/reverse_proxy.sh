e832f5bb76b3a0757df53572066f4eab8ea3c52d
Steffen Bleul <sbl@itbleul.de>
2016-02-24 13:56:34 +0100

    Added additional application properties.

--- imagescripts/reverse_proxy.sh
+++ imagescripts/reverse_proxy.sh
@@ -26,6 +26,7 @@ do
   REVERSE_PROXY_HOST_HEADER='$host'
   REVERSE_PROXY_HOST_HEADER_FORWARDED_FOR='$proxy_add_x_forwarded_for'
   REVERSE_PROXY_PROTO_HEADER='$scheme'
+  REVERSE_PROXY_UP_HEADER='$remote_addr'
 
   cat >> ${NGINX_DIRECTORY}/nginx.conf <<_EOF_
         location ${NGINX_PROXY_LOCATION} {
@@ -44,6 +45,7 @@ _EOF_
           proxy_set_header X-Forwarded-Host ${REVERSE_PROXY_HOST_HEADER};
           proxy_set_header X-Forwarded-Server ${REVERSE_PROXY_HOST_HEADER};
           proxy_set_header X-Forwarded-For ${REVERSE_PROXY_HOST_HEADER_FORWARDED_FOR};
+          proxy_set_header X-Real-IP ${REVERSE_PROXY_UP_HEADER};
 _EOF_
   fi
 
