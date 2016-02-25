d9e379caa6a6161df0aa0cd8155d5c0ef559f0ae
Steffen Bleul <sbl@itbleul.de>
2016-02-25 22:42:07 +0100

    Testing new reverse proxy setting.

--- imagescripts/reverse_proxy.sh
+++ imagescripts/reverse_proxy.sh
@@ -39,8 +39,10 @@ _EOF_
           proxy_set_header X_FORWARDED_PROTO ${REVERSE_PROXY_PROTO_HEADER};
           proxy_set_header X-Forwarded-Host ${REVERSE_PROXY_HOST_HEADER};
           proxy_set_header X-Forwarded-Server ${REVERSE_PROXY_HOST_HEADER};
-          proxy_set_header X-Forwarded-For ${REVERSE_PROXY_HOST_HEADER_FORWARDED_FOR};
+          proxy_set_header X-Forwarded-for ${REVERSE_PROXY_HOST_HEADER_FORWARDED_FOR};
           proxy_set_header X-Real-IP ${REVERSE_PROXY_UP_HEADER};
+          proxy_set_header Host ${REVERSE_PROXY_HOST_HEADER};
+          port_in_redirect off;
 _EOF_
   fi
 
