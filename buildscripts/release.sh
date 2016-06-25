97afe3ce4f69aca3fa323233f5bee05d1e715704
Steffen Bleul <sbl@itbleul.de>
2016-06-25 14:38:35 +0200

    NExt try

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -4,4 +4,4 @@
 # CONTAINER VARIABLES
 #------------------
 export NGINX_VERSION=1.10.1-r1
-export BUILD_BRANCH=$(git branch | grep -e "^*" | awk '/^\*/{print $2}')
+export BUILD_BRANCH=$(git branch | grep -e "^*" | cut -d' ' -f 2)
