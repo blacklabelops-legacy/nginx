85d380fc2626094188527741a629a8d63ab1487e
Steffen Bleul <sbl@itbleul.de>
2016-06-25 14:14:39 +0200

    Next try.

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -4,4 +4,4 @@
 # CONTAINER VARIABLES
 #------------------
 export NGINX_VERSION=1.10.1-r1
-export BUILD_BRANCH=$(git name-rev --name-only HEAD)
+export BUILD_BRANCH=$(git branch | awk '/^\*/{print $2}')
