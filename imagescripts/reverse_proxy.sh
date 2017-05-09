75dd4ed145940582ab52c9888756984ea9cc26be
Steffen Bleul <sbl@blacklabelops.com>
2017-05-09 10:54:54 +0200

    #24 request_uri should already contain path and args.

--- imagescripts/reverse_proxy.sh
+++ imagescripts/reverse_proxy.sh
@@ -166,7 +166,7 @@ do
 _EOF_
 
   REVERSE_PROXY_BACKEND_VARIABLE='$backend'
-  REVERSE_PROXY_BACKEND='$backend$request_uri$is_args$args'
+  REVERSE_PROXY_BACKEND='$backend$request_uri'
 
   if [ -n "${NGINX_PROXY_PASS}" ]; then
     cat >> $configFileReverseProxy/reverseProxy.conf <<_EOF_
